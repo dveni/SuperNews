910e24ba999f338244f450c7629d1eb15eaf2a7d
danivera <veranieto.dani@gmail.com>
2017-05-18 19:26:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/veran/supernews/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.veran.supernews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Intent intent = getIntent();
+    }
+}
---
+++ app/src/main/java/com/example/veran/supernews/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.veran.supernews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button boton = (Button) findViewById(R.id.button);
+
+        boton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"Change", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
