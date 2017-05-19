72c97d189c7a482331791c69441a018b079f0978
dveni <veranieto.dani@gmail.com>
2017-05-19 09:28:54 +0200

    Fin de pruebas

--- app/src/main/java/com/example/veran/supernews/MainActivity.java
+++ app/src/main/java/com/example/veran/supernews/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    int a;
         Button boton = (Button) findViewById(R.id.button);
 
         boton.setOnClickListener(new View.OnClickListener(){
